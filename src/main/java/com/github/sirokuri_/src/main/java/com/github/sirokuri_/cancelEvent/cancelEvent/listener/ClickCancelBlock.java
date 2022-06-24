9110b24a055ce7859532d948026700f009d5fec7
46kuri_ <ghost.46kuri@gmail.com>
2022-06-25 01:39:56 +0900

    [Fix&Add] Remake Plugin

---
+++ src/main/java/com/github/sirokuri_/src/main/java/com/github/sirokuri_/cancelEvent/cancelEvent/listener/ClickCancelBlock.java
@@ -0,0 +1,30 @@
+package com.github.sirokuri_.cancelEvent.cancelEvent.listener;
+
+import com.github.sirokuri_.cancelEvent.cancelEvent.CancelEvent;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
+
+public class ClickCancelBlock implements Listener {
+
+    private final CancelEvent plugin;
+
+    public ClickCancelBlock(CancelEvent cancelEvent) {
+        this.plugin = cancelEvent;
+    }
+
+    @EventHandler
+    public void onClick(PlayerInteractEvent event){
+        Block block = event.getClickedBlock();
+        if (block == null) return;
+        if (!(event.getAction() == Action.RIGHT_CLICK_BLOCK)) return;
+        if (!(event.getHand() == EquipmentSlot.HAND)) return;
+        if(block.getType() == Material.COMPOSTER){
+            event.setCancelled(true);
+        }
+    }
+}
---
+++ src/main/java/com/github/sirokuri_/src/main/java/com/github/sirokuri_/cancelEvent/cancelEvent/listener/CraftCancelItem.java
@@ -0,0 +1,54 @@
+package com.github.sirokuri_.cancelEvent.cancelEvent.listener;
+
+import com.github.sirokuri_.cancelEvent.cancelEvent.CancelEvent;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.PrepareItemCraftEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+
+public class CraftCancelItem implements Listener {
+
+    private final CancelEvent plugin;
+
+    public CraftCancelItem(CancelEvent cancelEvent) {
+        this.plugin = cancelEvent;
+    }
+
+    @EventHandler
+    public void onCraft(CraftItemEvent event) {
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onCraftHideItem(PrepareItemCraftEvent event){
+        event.getInventory().setResult(plugin.craftHideItem());
+    }
+
+    @EventHandler
+    public void onClick(InventoryClickEvent event) {
+        Player player = (Player) event.getView().getPlayer();
+        ItemStack itemStack = event.getCurrentItem();
+        int slot = event.getSlot();
+        if (itemStack == null) return;
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        if (itemMeta == null) return;
+        if (itemStack.getType() == Material.BARRIER) {
+            if(itemMeta.getDisplayName().equals(ChatColor.RED + "クラフトはできません")){
+                event.setCancelled(true);
+            }
+        }
+        if (slot == 8){
+            if (!(player.getGameMode() == GameMode.CREATIVE)){
+                event.setCancelled(true);
+            }
+        }
+    }
+}
